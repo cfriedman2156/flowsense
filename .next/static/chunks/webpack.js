fb4d52751e2ff6ad56bb80ce1aee973d15674e5f
[chase friedman] <[chaseafriedman@gmail.com]>
2024-12-23 15:50:51 -0800

    fixed navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b94820b1e9caefbf"; }
+/******/ 		__webpack_require__.h = function() { return "17d1f97e7b4b06bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
